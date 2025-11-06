fec65a17853b6c4d49655b7f0fc81fac563515d1
Larissa Yumi Ohashi <larissa.yumi@AD.UFABC.INT.BR>
2025-11-06 08:55:58 -0300

    Uploaded base files from the game

---
+++ player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	const SPEED = 5
+	
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_down"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_up"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.z += 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	move_and_slide()

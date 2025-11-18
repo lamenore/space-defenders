59511abe85206d484f4d923a86d4838632251cce
Lamenore <danilosanchesferme@gmail.com>
2025-11-18 01:06:35 -0300

    Game over screen - partial

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+func _on_play_again_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://phase1.tscn")
+	
+func _on_return_to_menu_btn_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")

0815b569e3b4c9681c697d7a5f90626e007796a0
Larissa Ohashi <lariyumi92@gmail.com>
2025-11-19 09:44:44 -0300

    Added new player movement inputs and shooting altered to manual shoot

--- player.gd
+++ player.gd
@@ -1,27 +1,29 @@
 extends CharacterBody3D
 
 @onready var projectile = preload("res://projectile.tscn") 
+@onready var timer = get_node("../Timer")
 
 const SPEED = 10
 
 var spawnedProjectiles: Array[Node] = []
+var can_shoot = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
 	
 func _physics_process(delta: float) -> void:
 	
 	var direction = Vector3.ZERO
 	
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down") or Input.is_action_pressed("ui_down"):
 		direction.y -= 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up") or Input.is_action_pressed("ui_up"):
 		direction.y += 1
 		
 	if direction != Vector3.ZERO:
@@ -30,20 +32,19 @@ func _physics_process(delta: float) -> void:
 	velocity.y = direction.y * SPEED
 	
 	move_and_slide()
+	
+func _on_timer_timeout():
+	can_shoot = true
 
-
-func _on_timer_timeout() -> void:
+func shoot() -> void:
+	can_shoot = false
 	var newProjectile = projectile.instantiate()
 	var spawnArea = get_node("ProjectileSpawnPoint")
-	var timer = get_node("../Timer")
-	
+		
 	newProjectile.rotation = spawnArea.rotation
 	get_tree().get_root().add_child(newProjectile)
 	newProjectile.global_position = Vector3(spawnArea.global_position.x, spawnArea.global_position.y, spawnArea.global_position.z + 3.0)
-	
+		
 	spawnedProjectiles.append(newProjectile)
 	
 	timer.start()
-
-func delete_projectiles() -> void:
-	pass
